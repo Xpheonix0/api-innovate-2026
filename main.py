3c4b207309a5c8f9cde26283299eeab5e769d313
Tech__erox <relic.of.sky@gmail.com>
2026-03-08 15:37:48 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Z-Engine Launcher
+"""
+
+import sys
+from PySide6.QtWidgets import QApplication
+from zengine.ui.main_window import MainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
